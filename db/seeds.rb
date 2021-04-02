eebbce38423bc40cc78fafb5935b227c90a4aa21
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-02 10:23:44 -0700

    Create seed file for parks and states.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    Park.destroy_all
+    State.destroy_all
+    seed = Seed.new
+    seed.generate_parks
+  end
+
+  def generate_parks
+    states_list = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+    type = ["State park", "National park"]
+    states_list.each do |state|
+      park_state = State.create!(name: state)
+      5.times do |i|
+        Park.create!(name: Faker::Name.middle_name, type: type.sample, state_id: park_state.id
+        )
+      end
+    end
+    puts "Generated #{State.count} states and #{Park.count} parks."
+  end
+end
+Seed.begin
