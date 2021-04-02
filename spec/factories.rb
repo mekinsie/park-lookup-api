f2c6fe1fddce5f1b44880b5cae0bd41555c44f2d
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-02 15:59:53 -0700

    Write get request spec for a single park.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,16 @@
 FactoryBot.define do
 
+
   factory(:park) do
+    State.create!(name: "Oregon")
     type_array = ["State park", "National park"]
     name {Faker::Name.middle_name }
     park_type { type_array.sample }
     state_id { 1 }
+  end
+
+  factory(:state) do
+    name { Faker::Address.state }
   end
 
 end
--- spec/requests/parks/get_parks_spec.rb
+++ spec/requests/parks/get_parks_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
-describe "the parks get request" do
+
+describe "the parks get request", :type => :request do
   let!(:parks) { FactoryBot.create_list(:park, 5)}
-
   before { get '/api/v1/states/1/parks'}
-
-  it 'returns all parks' do
-    expect(JSON.parse(response.body).size).to eq(5)
+  context 'when successful' do
+    it 'returns all parks' do
+      expect(JSON.parse(response.body).size).to eq(5)
+    end
+    it 'returns a status code 200' do
+      expect(response).to have_http_status(:success)
+    end
   end
 
+  describe "the park get request", :type => :request do
+    let!(:parks) { FactoryBot.create_list(:park, 5)}
+    before { get "/api/v1/states/1/parks/#{parks.first.id}"}
+    context 'when successful' do
+      it 'returns a park based on id' do
+        expect(JSON.parse(response.body)["name"]).to eq(parks.first.name)
+      end
 
+    end
+  end
 end
