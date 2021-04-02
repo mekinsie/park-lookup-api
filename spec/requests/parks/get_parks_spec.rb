945e33ed52a422a50f2347335e43fae36ad2e7df
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-02 16:03:41 -0700

    Write specs for when get call fails.

--- spec/requests/parks/get_parks_spec.rb
+++ spec/requests/parks/get_parks_spec.rb
@@ -1,26 +1,32 @@
 require 'rails_helper'
 
-
 describe "the parks get request", :type => :request do
+
   let!(:parks) { FactoryBot.create_list(:park, 5)}
   before { get '/api/v1/states/1/parks'}
+
   context 'when successful' do
     it 'returns all parks' do
       expect(JSON.parse(response.body).size).to eq(5)
     end
-    it 'returns a status code 200' do
-      expect(response).to have_http_status(:success)
+    it { expect(response).to have_http_status(200) }
+  end
+end
+
+describe "the park get request", :type => :request do
+
+  let!(:parks) { FactoryBot.create_list(:park, 5)}
+  before { get "/api/v1/states/1/parks/#{parks.first.id}"}
+
+  context 'when successful' do
+    it 'returns a park based on id' do
+      expect(JSON.parse(response.body)["name"]).to eq(parks.first.name)
     end
+    it { expect(response).to have_http_status(200) }
   end
 
-  describe "the park get request", :type => :request do
-    let!(:parks) { FactoryBot.create_list(:park, 5)}
-    before { get "/api/v1/states/1/parks/#{parks.first.id}"}
-    context 'when successful' do
-      it 'returns a park based on id' do
-        expect(JSON.parse(response.body)["name"]).to eq(parks.first.name)
-      end
-
-    end
+  context 'when not successful' do
+    before { get "/api/v1/states/1/parks/#{parks.last.id + 1}"}
+    it { expect(response).to have_http_status(404) }
   end
 end
