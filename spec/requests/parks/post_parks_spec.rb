c28bd8bf9b8d484105f82a9c3e9c0f0309f63506
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-02 17:06:29 -0700

    Write spec for posting a new park route.

---
+++ spec/requests/parks/post_parks_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the park patch request", :type => :request do
+
+  let!(:park) { FactoryBot.create(:park)}
+  let!(:state) {FactoryBot.create(:state)}
+  before { post "/api/v1/states/1/parks", params: { name: "Willamette", park_type: "State park", state_id: 1 }}
+
+  context 'when successful' do
+    it 'creates a new park' do
+      expect(JSON.parse(response.body)["message"]).to eq("You have successfully created a park.")
+    end
+    it { expect(response).to have_http_status(201) }
+  end
+
+  context 'when not successful' do
+    before { post "/api/v1/states/1/parks" }
+    it { expect(response).to have_http_status(422) }
+  end
+end
